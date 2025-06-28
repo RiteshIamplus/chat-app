04f0493e7a21f598bf08c3031c4301ccf61f7350
ritesh salve <riteshsalve07@gmail.com>
2025-06-28 18:22:25 +0530

    error solve meaisuop wala ok ok

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7m0gu4tsj5"
+    "revision": "0.eboi0853ei"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
