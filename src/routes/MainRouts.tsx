0f7334546bea8998ecb12f2639cfd2bd85a76cd1
ritesh salve <riteshsalve07@gmail.com>
2025-06-30 13:00:51 +0530

    some chabges dne

--- src/routes/MainRouts.tsx
+++ src/routes/MainRouts.tsx
@@ -8,9 +8,6 @@ import CreateGroupForm from "../Pages/CreateGroup";
 import PersonalChatPage from "@/Pages/PersonalChatPage";
 import RedirectIfLoggedIn from "./RedirectIfLoggedIn";
 import ProtectedRoute from "./ProtectedRoute";
-// import VideoCall from "@/Pages/mediasoup/VideoCall";
-// import AudioCall from "@/Pages/mediasoup/AudioCall";
-// import CallScreen from "@/Pages/mediasoup/CallScreen";
 import MediaCall from "@/Pages/webRTC/MediaCall";
 
 
