db9233a5dc4130254b9ff10780036addf3e85298
ritesh salve <riteshsalve07@gmail.com>
2025-06-30 12:54:12 +0530

    video call done from one side

--- src/Pages/mediasoup/CallScreen.tsx
+++ src/Pages/mediasoup/CallScreen.tsx
@@ -1,152 +1,152 @@
-import { useEffect, useRef, useState } from "react";
-import { useLocation } from "react-router-dom";
-import { io, Socket } from "socket.io-client";
-import * as mediasoupClient from "mediasoup-client";
+// import { useEffect, useRef, useState } from "react";
+// import { useLocation } from "react-router-dom";
+// import { io, Socket } from "socket.io-client";
+// import * as mediasoupClient from "mediasoup-client";
 
-const CallScreen = () => {
-  const location = useLocation();
-  const {
-    callerId,
-    receiverId,
-    incoming,
-    isVideo,
-  }: {
-    callerId: string;
-    receiverId: string;
-    incoming: boolean;
-    isVideo: boolean;
-  } = location.state;
+// const CallScreen = () => {
+//   const location = useLocation();
+//   const {
+//     callerId,
+//     receiverId,
+//     incoming,
+//     isVideo,
+//   }: {
+//     callerId: string;
+//     receiverId: string;
+//     incoming: boolean;
+//     isVideo: boolean;
+//   } = location.state;
 
-  const roomId = [callerId, receiverId].sort().join("-");
+//   const roomId = [callerId, receiverId].sort().join("-");
 
-  const socketRef = useRef<Socket>(
-    io("https://implusbackend-3xce.onrender.com", {
-      query: {
-        userId: incoming ? receiverId : callerId,
-        roomId,
-      },
-    })
-  );
+//   const socketRef = useRef<Socket>(
+//     io("https://implusbackend-3xce.onrender.com", {
+//       query: {
+//         userId: incoming ? receiverId : callerId,
+//         roomId,
+//       },
+//     })
+//   );
 
-  const localVideoRef = useRef<HTMLVideoElement>(null);
-  const remoteVideoRef = useRef<HTMLVideoElement>(null);
+//   const localVideoRef = useRef<HTMLVideoElement>(null);
+//   const remoteVideoRef = useRef<HTMLVideoElement>(null);
 
-  const [localStream, setLocalStream] = useState<MediaStream | null>(null);
-  const [remoteStream, setRemoteStream] = useState<MediaStream | null>(null);
-console.log(remoteStream)
-  const deviceRef = useRef<mediasoupClient.Device | null>(null);
-  const sendTransportRef = useRef<mediasoupClient.types.Transport | null>(null);
-  const recvTransportRef = useRef<mediasoupClient.types.Transport | null>(null);
+//   const [localStream, setLocalStream] = useState<MediaStream | null>(null);
+//   const [remoteStream, setRemoteStream] = useState<MediaStream | null>(null);
+// console.log(remoteStream)
+//   const deviceRef = useRef<mediasoupClient.Device | null>(null);
+//   const sendTransportRef = useRef<mediasoupClient.types.Transport | null>(null);
+//   const recvTransportRef = useRef<mediasoupClient.types.Transport | null>(null);
 
-  useEffect(() => {
-    initCall();
+//   useEffect(() => {
+//     initCall();
 
-    return () => {
-      socketRef.current.disconnect();
-      sendTransportRef.current?.close();
-      recvTransportRef.current?.close();
-      localStream?.getTracks().forEach((t) => t.stop());
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+//     return () => {
+//       socketRef.current.disconnect();
+//       sendTransportRef.current?.close();
+//       recvTransportRef.current?.close();
+//       localStream?.getTracks().forEach((t) => t.stop());
+//     };
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, []);
 
-  const initCall = async () => {
-    const stream = await navigator.mediaDevices.getUserMedia({
-      video: isVideo,
-      audio: true,
-    });
+//   const initCall = async () => {
+//     const stream = await navigator.mediaDevices.getUserMedia({
+//       video: isVideo,
+//       audio: true,
+//     });
 
-    setLocalStream(stream);
-    if (localVideoRef.current) localVideoRef.current.srcObject = stream;
+//     setLocalStream(stream);
+//     if (localVideoRef.current) localVideoRef.current.srcObject = stream;
 
-    const socket = socketRef.current;
+//     const socket = socketRef.current;
 
-    const rtpCapabilities: mediasoupClient.types.RtpCapabilities = await new Promise((res) =>
-      socket.emit("getRtpCapabilities", res)
-    );
+//     const rtpCapabilities: mediasoupClient.types.RtpCapabilities = await new Promise((res) =>
+//       socket.emit("getRtpCapabilities", res)
+//     );
 
-    const device = new mediasoupClient.Device();
-    await device.load({ routerRtpCapabilities: rtpCapabilities });
-    deviceRef.current = device;
+//     const device = new mediasoupClient.Device();
+//     await device.load({ routerRtpCapabilities: rtpCapabilities });
+//     deviceRef.current = device;
 
-    const sendTransportOptions = await new Promise<any>((res) =>
-      socket.emit("createSendTransport", res)
-    );
+//     const sendTransportOptions = await new Promise<any>((res) =>
+//       socket.emit("createSendTransport", res)
+//     );
 
-    const sendTransport = device.createSendTransport(sendTransportOptions);
+//     const sendTransport = device.createSendTransport(sendTransportOptions);
 
-    sendTransport.on("connect", ({ dtlsParameters }, cb) => {
-      socket.emit("connectTransport", { dtlsParameters, isConsumer: false }, cb);
-    });
+//     sendTransport.on("connect", ({ dtlsParameters }, cb) => {
+//       socket.emit("connectTransport", { dtlsParameters, isConsumer: false }, cb);
+//     });
 
-    sendTransport.on("produce", async ({ kind, rtpParameters }, cb) => {
-      const { id }: { id: string } = await new Promise((res) =>
-        socket.emit("produce", { kind, rtpParameters }, res)
-      );
-      cb({ id });
-    });
+//     sendTransport.on("produce", async ({ kind, rtpParameters }, cb) => {
+//       const { id }: { id: string } = await new Promise((res) =>
+//         socket.emit("produce", { kind, rtpParameters }, res)
+//       );
+//       cb({ id });
+//     });
 
-    sendTransportRef.current = sendTransport;
+//     sendTransportRef.current = sendTransport;
 
-    for (const track of stream.getTracks()) {
-      await sendTransport.produce({ track });
-    }
+//     for (const track of stream.getTracks()) {
+//       await sendTransport.produce({ track });
+//     }
 
-    socket.on("newProducer", async ({ producerId }: { producerId: string }) => {
-      const recvTransportOptions = await new Promise<any>((res) =>
-        socket.emit("createRecvTransport", res)
-      );
+//     socket.on("newProducer", async ({ producerId }: { producerId: string }) => {
+//       const recvTransportOptions = await new Promise<any>((res) =>
+//         socket.emit("createRecvTransport", res)
+//       );
 
-      const recvTransport = device.createRecvTransport(recvTransportOptions);
+//       const recvTransport = device.createRecvTransport(recvTransportOptions);
 
-      recvTransport.on("connect", ({ dtlsParameters }, cb) => {
-        socket.emit("connectTransport", { dtlsParameters, isConsumer: true }, cb);
-      });
+//       recvTransport.on("connect", ({ dtlsParameters }, cb) => {
+//         socket.emit("connectTransport", { dtlsParameters, isConsumer: true }, cb);
+//       });
 
-      const consumerParams = await new Promise<any>((res) =>
-        socket.emit(
-          "consume",
-          { producerId, rtpCapabilities: device.rtpCapabilities },
-          res
-        )
-      );
+//       const consumerParams = await new Promise<any>((res) =>
+//         socket.emit(
+//           "consume",
+//           { producerId, rtpCapabilities: device.rtpCapabilities },
+//           res
+//         )
+//       );
 
-      const consumer = await recvTransport.consume(consumerParams);
-      const remoteMediaStream = new MediaStream([consumer.track]);
+//       const consumer = await recvTransport.consume(consumerParams);
+//       const remoteMediaStream = new MediaStream([consumer.track]);
 
-      setRemoteStream(remoteMediaStream);
-      if (remoteVideoRef.current) {
-        remoteVideoRef.current.srcObject = remoteMediaStream;
-      }
+//       setRemoteStream(remoteMediaStream);
+//       if (remoteVideoRef.current) {
+//         remoteVideoRef.current.srcObject = remoteMediaStream;
+//       }
 
-      recvTransportRef.current = recvTransport;
-    });
-  };
+//       recvTransportRef.current = recvTransport;
+//     });
+//   };
 
-  return (
-    <div className="h-screen w-full flex flex-col items-center justify-center bg-gray-900 text-white p-4 space-y-4">
-      <h2 className="text-xl">{isVideo ? "Video" : "Audio"} Call</h2>
-      <div className="flex gap-4">
-        <video
-          ref={localVideoRef}
-          autoPlay
-          muted
-          className="w-48 h-48 border rounded-xl"
-        />
-        <video
-          ref={remoteVideoRef}
-          autoPlay
-          className="w-48 h-48 border rounded-xl"
-        />
-      </div>
-      <button
-        onClick={() => window.history.back()}
-        className="bg-red-600 text-white px-6 py-2 rounded-xl"
-      >
-        End Call
-      </button>
-    </div>
-  );
-};
+//   return (
+//     <div className="h-screen w-full flex flex-col items-center justify-center bg-gray-900 text-white p-4 space-y-4">
+//       <h2 className="text-xl">{isVideo ? "Video" : "Audio"} Call</h2>
+//       <div className="flex gap-4">
+//         <video
+//           ref={localVideoRef}
+//           autoPlay
+//           muted
+//           className="w-48 h-48 border rounded-xl"
+//         />
+//         <video
+//           ref={remoteVideoRef}
+//           autoPlay
+//           className="w-48 h-48 border rounded-xl"
+//         />
+//       </div>
+//       <button
+//         onClick={() => window.history.back()}
+//         className="bg-red-600 text-white px-6 py-2 rounded-xl"
+//       >
+//         End Call
+//       </button>
+//     </div>
+//   );
+// };
 
-export default CallScreen;
+// export default CallScreen;
